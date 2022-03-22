63d9e2cd736f11c7c04d84ac7a58aebc74666bb2
Jen Truong <jennifertruong09@gmail.com>
2022-03-22 10:07:17 -0400

    added additional file for home landing page and added some code to populate page based on what user clicks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,29 @@
 // Header Page that holds logo and nav bar?
 import React, { useState } from 'react';
+// import other components ->
 
 export default function Header() {
     // state for what the current page is and page is changed
+    const [currentPage, pageChange] = useState('Home');
+
+    // what page will render based on what is clicked
+    const renderPage = () => {
+        // switch case statment for all pages - make sure case name is exactly the same as the tabs later in Nav.js
+        switch(currentPage) {
+            case "Home":
+                return <Home></Home>
+            case "About":
+                return <About></About>
+            case "Work":
+                return <Work></Work>
+            case "Contact":
+                return <Contact></Contact>
+            case "Resume":
+                return <Resume></Resume>
+            default:
+                return <Home></Home>
+        }
+    };
+
+    // return to renderPage
 }
