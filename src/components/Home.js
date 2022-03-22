ecbc4edf6d03e5c23285431d79cbfab6e88be1aa
Jen Truong <jennifertruong09@gmail.com>
2022-03-22 13:41:28 -0400

    added a basic layout of text to the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,1 +1,16 @@
-// Landing page 
+// Landing page
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div className='whoAmI'>
+            <div className='nameAndTitle'>
+                <p>Hello! I'm</p>
+                <h3 className='name'>Jennifer Truong,</h3>
+                <h3 className='title'>Full Stack Web Developer.</h3>
+                <p>I like to build things for the web.</p>
+            </div>
+
+        </div>
+    )
+}
