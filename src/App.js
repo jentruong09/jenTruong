218a939db726ee4bf846c08a0058c6dad243c5ee
Jen Truong <jennifertruong09@gmail.com>
2022-03-22 14:05:26 -0400

    added navbar tabs and info, will change layout later. added footer to app.js and notes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 
+// not sure if Footer is needed here?
+
 function App() {
   return (
     <div>
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,1 +1,19 @@
 // Navigation bar and working links
+import React from 'react';
+
+export default function Nav() {
+    const tabs = ['Home', 'About', 'Work', 'Contact', 'Resume'];
+
+    return (
+        <ul className='navBar'>
+            {tabs.map(tab => (
+                <li className='navItem' key={tab}>
+                    {/* // ternary operator for link to page depending on which page user is on -- fix later if issues? */}
+                    <a href={'#' + tab} onClick={() => props.handlePageChange(tab)} className={props.currentPage === tab ? 'nav-link active' : 'nav-link'}>
+                        {tab}
+                    </a>
+                </li>
+            ))}
+        </ul>
+    )
+}
