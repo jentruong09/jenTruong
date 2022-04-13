51b1cb975920e4c4fecfa313f597c903faa50618
Jen Truong <jennifertruong09@gmail.com>
2022-04-13 19:58:30 -0400

    made changes to my about me page

--- src/components/About.js
+++ src/components/About.js
@@ -8,8 +8,8 @@ export default function About() {
             <div className='about'>
                 <p>
                     Welcome! My name is Jennifer Truong, you can call me Jen! I have a background in Accounting and after a few years in the field,
-                    I decided I wanted to take on my next challenge, web development. Through web development, I am able to create web designs that incorporates my interests and creativity.
-                    I am a full-stack web developer based in New York City and I enjoy designing and building applications for the web.
+                    I decided I wanted to take on my next challenge, web development. Through web development, I am able to create web applications that incorporates my interests and creativity.
+                    I am a full-stack web developer based in New York City and I enjoy designing and building better experiences on the web.
                 </p>
             </div>
             <div className='aboutPic'>
