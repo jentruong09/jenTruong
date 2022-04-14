5d8558d1b9ab4e7e451cf5eb8ef7f1b625fb8c6a
Jen Truong <jennifertruong09@gmail.com>
2022-04-14 12:06:08 -0400

    cleaned up some of the code

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,60 +5,9 @@ import '../styles/Contact.css'
 
 
 export default function Contact() {
-    // const [formState, setFormState] = useState({ name: "", email: "", message: "" });
-
-    // const { name, email, message } = formState;
-    // const [errorMessage, setErrorMessage] = useState("")
-
-    // function handleChange(el) {
-    //     if (el.target.name === 'email') {
-    //         const validEmail = validateEmail(el.target.value);
-    //         if (!validEmail) {
-    //             setErrorMessage('You need to enter a vaild email.');
-    //         } else {
-    //             setErrorMessage('');
-    //         }
-    //     } else {
-    //         if (!el.target.value.length) {
-    //             setErrorMessage(`${el.target.name} is required.`);
-    //         } else {
-    //             setErrorMessage('');
-    //         }
-    //     }
-    //     if (!errorMessage) {
-    //         setFormState({ ...formState, [el.target.name]: el.target.value });
-    //     }
-    // }
-
-    // function handleSubmit(el) {
-    //     el.preventDefault();
-    //     console.log(formState)
-    // }
 
     return (
         <div className='contactArea'>
-            {/* <h3> Contact me: </h3> */}
-            {/* <form onSubmit={handleSubmit}>
-                <div className='form-group'>
-                    <label htmlFor='name'>Name </label>
-                    <input type='text' className="form-control" dafaultvalue={name} onBlur={handleChange} name='name' />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="email">Email address </label>
-                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" dafaultvalue={email} onBlur={handleChange} name='email' />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="message">Message </label>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="6" dafaultvalue={message} onBlur={handleChange} name='message'></textarea>
-                </div>
-                {errorMessage && (
-                    <div>
-                        <p className='errorMessage'>{errorMessage}</p>
-                    </div>
-                )}
-                <button type='submit' className='submit btn btn-dark' onSubmit={handleSubmit}>Submit</button>
-            </form> */}
-
             <div className='otherContact'>
                 <h3> You can contact me here: </h3>
                 <div className="full-content" id="contact">
