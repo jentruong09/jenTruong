5e5425bb658a760e6e5b02f63057e48b2d9cbb77
Jen Truong <jennifertruong09@gmail.com>
2022-05-19 10:14:43 -0400

    made changes to the contact page so users can see usernames and emails

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,17 +8,20 @@ export default function Contact() {
     return (
         <div className='contactArea'>
             <div className='otherContact'>
-                <h3> You can contact me here: </h3>
+                <h3 className='contactTitle'> Contact me here: </h3>
                 <div className="full-content" id="contact">
                     <ul className='contactList'>
-                        <li>
-                            <a href="mailto: jennifertruong09@gmail.com"><img className='contactImg' src='https://i.imgur.com/hi8b397.png' alt='email' /></a>
+                        <li className='indContact'>
+                            <a href="mailto: jennifertruong09@gmail.com"><img className='contactImg' src='https://i.imgur.com/0CBz6xh.png' alt='email' /></a>
+                            <a href="mailto: jennifertruong09@gmail.com" className='wordLinks'> <p> jennifertruong09@gmail.com </p></a>
                         </li>
-                        <li>
+                        <li className='indContact'>
                             <a href="https://github.com/jentruong09"><img className='contactImg' src='https://i.imgur.com/K74n3yJ.png' alt='github' /></a>
+                            <a href="https://github.com/jentruong09" className='wordLinks'> <p> jentruong09 </p></a>
                         </li>
-                        <li>
+                        <li className='indContact'>
                             <a href="https://www.linkedin.com/in/jennifertruong09/"><img className='contactImg' src='https://i.imgur.com/sq41iit.png' alt='linkedin' /></a>
+                            <a href="https://www.linkedin.com/in/jennifertruong09/" className='wordLinks'> <p> jennifertruong09 </p></a>
                         </li>
                     </ul>
                 </div>
