291a63e60d688c32918da462b7e1a1b628b90e89
Jen Truong <jennifertruong09@gmail.com>
2022-05-24 11:58:36 -0400

    added another proficiency

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -56,6 +56,9 @@ export default function Resume() {
                         Express.js
                     </li>
                     <li>
+                        React.js
+                    </li>
+                    <li>
                         MySQL
                     </li>
                     <li>
